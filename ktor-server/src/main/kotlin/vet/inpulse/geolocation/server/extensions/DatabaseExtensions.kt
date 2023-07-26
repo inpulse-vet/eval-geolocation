98d4ca361a23bb4c484622ee55bbbfb68e4e5bf9
Mathews Boscolo <mathewsboscolo@outlook.com>
2023-07-26 10:19:30 -0300

    - add extension function to convert ResultRow to RestaurantDetails
     - Improve code readability by adding an extension function

--- ktor-server/src/main/kotlin/vet/inpulse/geolocation/server/extensions/DatabaseExtensions.kt
+++ ktor-server/src/main/kotlin/vet/inpulse/geolocation/server/extensions/DatabaseExtensions.kt
@@ -1,11 +1,13 @@
 package vet.inpulse.geolocation.server.extensions
 
+import kotlinx.datetime.LocalTime
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.statements.BatchInsertStatement
 import org.jetbrains.exposed.sql.statements.InsertStatement
 import org.postgis.Point
 import vet.inpulse.geolocation.*
 import vet.inpulse.geolocation.server.database.RestaurantTable
+import java.util.UUID
 
 fun BatchInsertStatement.parseBatch(restaurantDetails: RestaurantDetails) {
     this[RestaurantTable.id] = restaurantDetails.id
@@ -45,6 +47,20 @@ fun ResultRow.toRestaurant(): Restaurant {
         id = this[RestaurantTable.id],
         name = this[RestaurantTable.name],
         location = Location(latitude, longitude)
+    )
+}
+
+fun ResultRow.toRestaurantDetails(restaurantId: UUID): RestaurantDetails {
+    val rawLocation = this[RestaurantTable.location]
+    val location = Location(Latitude(rawLocation.x.toFloat()), Longitude(rawLocation.y.toFloat()))
+
+    val closingTime = LocalTime.parse(this[RestaurantTable.closingTime])
+    val openingTime = LocalTime.parse(this[RestaurantTable.openingTime])
+
+    return RestaurantDetails(
+        restaurantId, this[RestaurantTable.name], location,
+        this[RestaurantTable.streetAddress], this[RestaurantTable.phone],
+        this[RestaurantTable.website], OpenHours(openingTime, closingTime)
     )
 }
 
--- ktor-server/src/main/kotlin/vet/inpulse/geolocation/server/repository/RestaurantRepositoryImpl.kt
+++ ktor-server/src/main/kotlin/vet/inpulse/geolocation/server/repository/RestaurantRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package vet.inpulse.geolocation.server.repository
 
-import kotlinx.datetime.LocalTime
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.postgis.Point
@@ -10,6 +9,7 @@ import vet.inpulse.geolocation.server.database.distance
 import vet.inpulse.geolocation.server.extensions.parseBatch
 import vet.inpulse.geolocation.server.extensions.parseInsert
 import vet.inpulse.geolocation.server.extensions.toRestaurant
+import vet.inpulse.geolocation.server.extensions.toRestaurantDetails
 import vet.inpulse.server.RestaurantRepository
 import java.util.*
 
@@ -37,19 +37,7 @@ class RestaurantRepositoryImpl : RestaurantRepository {
         return newSuspendedTransaction {
             RestaurantTable.select {
                 RestaurantTable.id eq restaurantId
-            }.singleOrNull()?.let { result ->
-                val rawLocation = result[RestaurantTable.location]
-                val location = Location(Latitude(rawLocation.x.toFloat()), Longitude(rawLocation.y.toFloat()))
-
-                val closingTime = LocalTime.parse(result[RestaurantTable.closingTime])
-                val openingTime = LocalTime.parse(result[RestaurantTable.openingTime])
-
-                RestaurantDetails(
-                    restaurantId, result[RestaurantTable.name], location,
-                    result[RestaurantTable.streetAddress], result[RestaurantTable.phone],
-                    result[RestaurantTable.website], OpenHours(openingTime, closingTime)
-                )
-            }
+            }.singleOrNull()?.toRestaurantDetails(restaurantId)
         }
     }
 
